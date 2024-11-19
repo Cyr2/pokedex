d4a22edf2d3f2e943612e5b80cf3cfaef7427305
Cyri | Darius <85651634+CyriGolo@users.noreply.github.com>
2024-11-20 00:11:31 +0100

    Add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      color: {
+        normal: '#BCBCAC',
+        fighting: '#BC5442',
+        flying: '#669AFF',
+        poison: '#AB549A',
+        ground: '#DEBC54',
+        rock: '#BCAC66',
+        bug: '#ABBC1C',
+        ghost: '#6666BC',
+        steel: '#ABACBC',
+        fire: '#FF421C',
+        water: '#2F9AFF',
+        grass: '#78CD54',
+        electric: '#FFCD30',
+        psychic: '#FF549A',
+        ice: '#78DEFF',
+        dragon: '#7866EF',
+        dark: '#785442',
+        fairy: '#FFACFF',
+        shadow: '#0E2E4C'
+      }
+    }
+  },
+};
