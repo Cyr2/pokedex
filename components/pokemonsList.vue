0a2a07365c51bc8a5f552cef7c09dfe554d5037e
Cyri | Darius <85651634+CyriGolo@users.noreply.github.com>
2024-11-20 00:11:08 +0100

    Update pokemon import

--- components/pokemonsList.vue
+++ components/pokemonsList.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { pokemons, loaded } from '~/hooks/fetchPokemon';
-
 const props = defineProps<{
+  pokemons: [],
   search: string,
   handleSelect: (value: number) => void
 }>();
+
+const pokemons = ref(props.pokemons);
 
 const filtredPokemons = computed(() => {
   return pokemons.value.filter((pokemon) => {
@@ -14,16 +15,16 @@ const filtredPokemons = computed(() => {
 </script>
 
 <template>
-  <ul class="flex flex-wrap gap-6" v-if="loaded">
-    <li v-for="pokemon in filtredPokemons" :key="pokemon.id" @click="props.handleSelect(pokemon.id)" class="bg-white relative flex flex-col p-4 pt-14 cursor-pointer group">
-      <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`" :alt="pokemon.name" class="absolute max-w-24 top-0 left-1/2 -translate-x-1/2 -translate-y-14 group-hover:scale-110">
-      <p>N°{{ pokemon.id }}</p>
-      <h3>
+  <ul class="flex flex-wrap gap-6 gap-y-20">
+    <li v-for="pokemon in filtredPokemons" :key="pokemon.id" @click="props.handleSelect(pokemon.id)" class="bg-white relative flex flex-col items-center font-bold p-6 pt-10 rounded-2xl shadow-xl cursor-pointer min-w-[23%] flex-[0.5] duration-[100ms] group hover:outline outline-2 outline-gray-300">
+      <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`" :alt="pokemon.name" class="absolute max-w-24 top-0 left-1/2 -translate-x-1/2 -translate-y-14 duration-[100ms] group-hover:scale-110">
+      <p class="text-xs text-gray-500">N°{{ pokemon.id }}</p>
+      <h3 class="text-lg first-letter:uppercase">
         {{ pokemon.name }}
       </h3>
-      <ul class="flex gap-2">
-        <li v-for="type in pokemon.types">
-          <p>{{ type }}</p>
+      <ul class="flex gap-2 mt-3">
+        <li v-for="type in pokemon.types" class="py-1 px-2 rounded-md bg-normal">
+          <p class="first-letter:uppercase text-sm text-gray-600">{{ type }}</p>
         </li>
       </ul>
     </li>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <template>
-  <main class="min-h-screen flex gap justify-between gap-6 py-8 px-[10vw]">
+  <main class="min-h-screen flex gap justify-between gap-6 py-8 px-[10vw]" v-if="loaded">
     <div class="flex flex-col w-full gap-20">
       <SearchBar
         :handleSearch="handleSearch"
       />
       <PokemonsList
+        :pokemons="pokemons"
         :search="search"
         :handleSelect="handleSelect"
       />
     </div>
-    <div class="w-80">
+    <div class="w-80 shrink-0 max-lg:hidden">
       <SelectedPokemon
         :selectedPokemon="selectedPokemon"
       />
     </div>
   </main>
+  <div v-else class="w-screen h-screen flex items-center justify-center">
+    <img src="/pokeball-icon.png" alt="Pokeball" class="animate-spin brightness-75 w-14">
+  </div>
 </template>
 
 <script setup lang="ts">
+import { pokemons, loaded } from '~/hooks/fetchPokemon';
+
 const search = ref('');
 
 const handleSearch = (value: string) => {
